aa0c06ce4d930436dbbf953df86a734c178cfea8
waren2 <warenodhiambo2@gmail.com>
2025-05-11 10:30:21 -0400

    commits

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,108 @@
 //}
 //if(34>1 && 2<1){
  //   console.log("these are equal");
- 
+
+
+
+//var marks = 10;
+//for(var x = 0; x <= marks;x++){
+    //console.log(x);
+//}
+//var students = ['wareb','john','james','abel'];
+//for(var list = 0; list < students.length;list++){
+   // console.log(students[list]);}
+//var names = ["waren","jod","wayne","jared"];
+//for(var x = 0; x < names.length;x++){
+   // console.log(names[x]);}
+
+// var length1 = 13;
+// var width1 = 10;
+// var area = length1*width1;
+// console.log(area);
+//  var area1 = area(10,15);
+//  function area(length, width){
+//   return length * width;
+//  }
+// function area(length,width){
+//   return length*width;
+// }
+// var area1 = area(10,15);
+// console.log(area1);
+// function area(pie,radius){
+//   return pie*radius*radius;
+
+// }
+// var area2 = area(3.142,21);
+// console.log(area2);
+
+// function area(length,width){
+//   return length*width;
+// }
+// var areas = [];
+// areas.push(area(20,1));
+// areas.push(area(10,10));
+// areas.push(area(10,1));
+// areas.push(area(1000,2));
+
+// console.log(areas);
+
+
+// var area2 = [];
+// area2.push(area(10,200));
+// area2.push(area(20,20));
+// area2.push(area(10,20));
+// function area(length,width){
+//   return length*width;
+// }
+// console.log(area2);
+
+// var myAccountBalance = 1000;
+// function makeTransactions(priceOfSales){
+//   if(priceOfSales <= myAccountBalance){
+//     myAccountBalance -= priceOfSales;
+//     console.log("purchase succefull!");
+//   }else{
+//     console.log("insufficient balance");
+//   }
+// }
+// console.log(myAccountBalance);
+// makeTransactions(100);
+// console.log(myAccountBalance);
+
+
+// makeTransactions(20.29);
+// console.log(myAccountBalance);
+// makeTransactions(29.67);
+// console.log(myAccountBalance);
+// makeTransactions(23.10);
+// console.log(myAccountBalance);
+// makeTransactions(10.11);
+// console.log(myAccountBalance);
+// makeTransactions(20001);
+//  var banlBalance = 1000;
+// var transactions = function(budget){
+//   if(budget <= banlBalance){
+//     banlBalance -= budget;
+//     console.log("procede to pay")
+//   }else{
+//     console.log("insufficient funds ,sorry!");
+//   }
+// }
+// transactions(10.00);
+// transactions(20.20);
+// transactions(50.00);
+
+var customersName = function(first,last){
+  console.log("first name: " + fist + "last" + last);
+}
+customersName(waren ,odhiambo);
+
+
+
+
+
+
+
+
+
+
