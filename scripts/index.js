08e0dce21cb3c32fa6314ec7f583fe34ded5e5da
waren2 <warenodhiambo2@gmail.com>
2025-05-11 06:59:12 -0400

    commits

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,28 @@
-var myAccountBalance = 30000;
-var myCar = 2000099;
+//var myAccountBalance = 30000;
+//var myCar = 200;
+//var motor = 100;
 
 
-if(myCar <= myAccountBalance){
-    myAccountBalance -= myCar;
-    console.log("we can purchase this please mum ,");
+//if(myCar <= myAccountBalance){
+  //  myAccountBalance -= myCar;
+    //console.log("well");
+    //console.log('my account balance is ,' + myAccountBalance); 
+//} else if(myCar - motor <= myAccountBalance){
+//    console.log('hey ,bro let purchase ,')
+//}
+//else{
+  //  console.log("top up your account ,you have inadequete funds to complete the payment")
+//}
+//var val1 = "34";
+//var val2 = "34";
+//if(val1 === val2){
+  //  console.log("these are the same");
+//}else{
+//    console.log("these are not the same");
+//}
+//if(1===1 && 2 <3){
+//    console.log("these are both true.")
+//}
+//if(34>1 && 2<1){
+ //   console.log("these are equal");
  
-}else{
-    console.log("top up your account ,you have inadequete funds to complete the payment")
-}
