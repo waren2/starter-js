f4bae93acc323fa3ac21ea779287020ef753b38e
waren2 <warenodhiambo2@gmail.com>
2025-05-11 05:08:52 -0400

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+var myAccountBalance = 30000;
+var myCar = 2000099;
+
+
+if(myCar <= myAccountBalance){
+    myAccountBalance -= myCar;
+    console.log("we can purchase this please mum ,");
+ 
+}else{
+    console.log("top up your account ,you have inadequete funds to complete the payment")
+}
